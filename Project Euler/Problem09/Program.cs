5087c31985c3196746e1f35376d95794d3769542
henrik molnes <molnes@gmail.com>
2012-09-17 21:50:36 +0200

    Problem 09 updated with Euclid's formula

--- Project Euler/Problem09/Program.cs
+++ Project Euler/Problem09/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Problem9
 {
-    //A Pythagorean triplet is a set of three natural numbers, a  b  c, for which,
+    //A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
     //a^2 + b^2 = c^2
     //For example, 3^2 + 4^2 = 9 + 16 = 25 = 52.
     //There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc.
@@ -13,7 +13,24 @@ namespace Problem9
     {
         static void Main(string[] args)
         {
+            double a, b, c; a = b = c = 0;
+            bool loop = true;
+            
+            for (int x = 0; loop; x++)
+            {
+                for (int y = 0; y < x && loop; y++)
+                {
+                    a = Math.Pow(x, 2) - Math.Pow(y, 2);
+                    b = 2 * x * y;
+                    c = Math.Pow(x, 2) + Math.Pow(y, 2);
 
+                    if (a + b + c == 1000 && Math.Pow(a, 2) + Math.Pow(b, 2) == Math.Pow(c, 2))
+                        loop = false;
+                }
+            }
+            
+            Console.WriteLine("{0}", a * b * c);
+            Console.ReadKey();
         }
     }
 }
